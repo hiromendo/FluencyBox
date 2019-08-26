5ebd9389653af148454d2bd02c4ab136e1d89bdb
Kenzo Mendoza <kenzom954@gmail.com>
2019-08-25 17:27:36 -0700

    added loading props in PrivateRoute component

--- front-end-side/src/util/PrivateRoute.jsx
+++ front-end-side/src/util/PrivateRoute.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, authInfo, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    authInfo.isAuthenticated === true
-      ? <Component {...props} />
-      : <Redirect to={{
-          pathname: '/login',
-          state: { from: props.location }
-        }} />
-  )} />
-)
+const PrivateRoute = ({ component: Component, authInfo, loading, ...rest }) => {
+  
+  if (loading) {
+    return <h3>Loading....</h3>
+  }
+
+  return (
+    <Route {...rest} render={(props) => (
+      authInfo.isAuthenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{
+            pathname: '/login',
+            state: { from: props.location }
+          }} />
+    )} />
+  )
+}
 
 
-const mapStateToProps = ({ authInfo }) => ({
-  authInfo
+const mapStateToProps = ({ authInfo, loading }) => ({
+  authInfo,
+  loading
 })
 
 export default connect(mapStateToProps)(PrivateRoute)
