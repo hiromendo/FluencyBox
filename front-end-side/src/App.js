26f597e188501899fee87d75e39ef451b10e79ec
Kenzo Mendoza <kenzom954@gmail.com>
2019-08-29 22:10:24 -0700

    added navbar

--- front-end-side/src/App.js
+++ front-end-side/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link, withRouter } from 'react-router-dom';
 
 import { HomePage, LoginPage , AppLayOut, UserProfilePage } from './components/pages';
 import PrivateRoute from './util/PrivateRoute';
-import { endLoading, getCurrentUser, getAccessToken } from './actions';
+import { endLoading, getCurrentUser, getAccessToken, removeCurrentUser } from './actions';
 
 import "./App.scss";
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.logOffUser = this.logOffUser.bind(this);
+    this.renderNavBar = this.renderNavBar.bind(this);
+  }
+
   componentDidMount() {
     if (localStorage.access_token && localStorage.uid) {
       const infoObj = {};
@@ -21,10 +28,33 @@ class App extends React.Component {
     }
   }
 
+  logOffUser() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    this.props.removeCurrentUser();
+  }
+
+  renderNavBar() {
+    return (
+    <nav id="top-nav">
+      <ul>
+        <li><Link to="/app">Dashboard</Link></li>
+        <li><Link to="/userprofile">UserProfile</Link></li>
+        <li><Link to="/aboutus">About Us</Link></li>
+        <li><a href="#" onClick={this.logOffUser}>Logout</a></li>
+      </ul>
+    </nav>
+    )
+  }
+
   render() {
+    const { location } = this.props;
     return (
       <div className="App">
+      <header>
         <h1>Fluency Box</h1>
+        {location.pathname !== "/login" ? this.renderNavBar() : null}
+      </header>
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/login" component={LoginPage}/>
@@ -40,7 +70,8 @@ class App extends React.Component {
 const mapDispatchToProps = {
   endLoading,
   getCurrentUser,
-  getAccessToken
+  getAccessToken,
+  removeCurrentUser
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default withRouter(connect(null, mapDispatchToProps)(App));
--- front-end-side/src/components/pages/appLayOut.jsx
+++ front-end-side/src/components/pages/appLayOut.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { removeCurrentUser } from '../../actions';
-
 
 class AppLayOut extends React.Component {
-
-  logOffUser() {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('refresh_token');
-    this.props.removeCurrentUser();
-  }
 
   render() {
     return (
       <div>
-        <h2>App Layout</h2>
-        <Link to="/userprofile">Click here to update your profile</Link>
-        <div onClick={this.logOffUser.bind(this)}>
-          <a href="#/">Click here to log off</a>
-        </div>
+        <main>
+          <h2>App Dashboard</h2>
+        </main>
       </div>
     )
   }
 }
 
-const mapDispatchToProps = {
-  removeCurrentUser
-}
+const mapStateToProps = ({authInfo}) => ({
+  userInfo: authInfo.serverResponse.user
+})
 
-export default connect(null, mapDispatchToProps)(AppLayOut);
+
+export default connect(mapStateToProps)(AppLayOut);
