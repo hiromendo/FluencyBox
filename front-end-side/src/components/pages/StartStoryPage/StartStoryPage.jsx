646097b0c0dbcfde7084f2ce1649f62b66276c75
Kenzo Mendoza <kenzom954@gmail.com>
2019-09-21 16:23:01 -0700

    completed layout for desktop story content

--- front-end-side/src/components/pages/StartStoryPage/StartStoryPage.jsx
+++ front-end-side/src/components/pages/StartStoryPage/StartStoryPage.jsx
@@ -1,45 +1,103 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import './StartStoryPage.scss';
 
 class StartStoryPage extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.displayDesktopLayout = this.displayDesktopLayout.bind(this);
+    this.displayMobileLayOut = this.displayMobileLayOut.bind(this);
     this.state = {
-      page: false
+      isMobile: true
     }
+  }
+
+  displayDesktopLayout() {
+    const { uid } = this.props
+    return (
+      <Grid>
+        <Row middle="md">
+          <Col md={3} mdOffset={1}>
+            <div class="btn btn-dark-blue">Repeat Audio</div>
+          </Col>
+          <Col md={4} mdOffset={1}>
+            <div class="btn btn-dark-blue">Hide Subtitles</div>
+          </Col>
+          <Col md={1}>
+            <div class="btn btn-dark-blue">Restart</div>
+          </Col>
+          <Col md={1}>
+            <div class="btn btn-dark-blue">
+              <Link to={`/story/${uid}`}>Home</Link>
+            </div>
+          </Col>
+        </Row>
+        <Row center="md">
+          <Col md={12}>
+            <div id="story-content">Content Here</div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={3} mdOffset={1}>
+          <div class="btn btn-dark-blue">
+            Record Again
+          </div>
+          </Col>
+          <Col md={2} mdOffset={1}>
+            <div class="btn btn-dark-blue">
+              Record Button
+            </div>
+          </Col>
+          <Col md={3} mdOffset={2}>
+            <div class="btn btn-dark-blue">
+              Next Scene
+            </div>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+
+  displayMobileLayOut() {
+    const { uid } = this.props
+    return (
+      <Grid>
+        <Row middle="xs">
+          <Col xs={4} xsOffset={1}>
+            <div className="btn btn-dark-blue">Repeat Audio</div>
+          </Col>
+          <Col xs={4} xsOffset={2}>
+            <div className="btn btn-dark-blue">Hide Subtitles</div>
+          </Col>
+        </Row>
+        <Row center="xs">
+          <Col xs={12}>
+            <div id="story-content">Content Here</div>
+          </Col>
+        </Row>
+        <Row center="xs" middle="xs">
+          <Col xs={3}>
+            <div className="btn btn-dark-blue">Restart</div>
+          </Col>
+          <Col xs={4} xsOffset={1}>
+            Record Button
+          </Col>
+          <Col xs={3} xsOffset={1}>
+          <div className="btn btn-dark-blue">
+            <Link to={`/story/${uid}`}>Home</Link>
+          </div>
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
 
   render() {
     return (
       <div id="story-start">
-        <Grid className="content-container">
-          <Row middle="xs">
-            <Col xs={4} xsOffset={1}>
-              <div>Repeat Audio</div>
-            </Col>
-            <Col xs={4} xsOffset={2}>
-              <div>Hide Subtitles</div>
-            </Col>
-          </Row>
-          <Row center="xs">
-            <Col xs={12}>
-              <div style={{border: '1px solid red'}}>Content Here</div>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={2} xsOffset={1}>
-              Restart
-            </Col>
-            <Col xs={4} xsOffset={1}>
-              Record Button
-            </Col>
-            <Col xs={2} xsOffset={1}>
-              Home
-            </Col>
-          </Row>
-        </Grid>
+        {this.state.isMobile ? this.displayMobileLayOut() : this.displayDesktopLayout()}
       </div>
     )
   }
