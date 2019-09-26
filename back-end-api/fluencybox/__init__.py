18ff68f9cb81b0aed098b8a919336f9e5b14f680
Sanjay <sanjum308@hotmail.com>
2019-09-26 07:38:02 +0300

    added default admin

--- back-end-api/fluencybox/__init__.py
+++ back-end-api/fluencybox/__init__.py
@@ -22,4 +22,13 @@ if not database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
         db.session.commit()
         print("Dummy record inserted for Dev only - email - hiromendo@gmail.com & password - 12345")
 
+if app.config['DEBUG']==1:
+    from fluencybox.models import User
+    admin = User.query.filter_by(user_name = 'admin').first()
+    if not admin:
+        admin_user = User(uid='1c9f4d3c-72b6-4cab-b06b-14a82bd025b2', first_name = 'Admin', last_name = 'M', user_name = 'admin', email_address = 'admin@gmail.com', password = 'sha256$ID5K2eg5$1fdfd6afa111dae5599398ab0a0fb367e088e9d72b6dad887e5541d6727e9fa3', phone_number = '+1 23456789', is_admin = 1)
+        db.session.add(admin_user)
+        db.session.commit()
+        print("Admin user inserted for Dev only - email - admin@gmail.com & user_name - admin & password - 12345")
+
 from fluencybox import routes
