f6c3155b2497607d9a06d021391f998bc0a3a419
Kenzo Mendoza <kenzom954@gmail.com>
2020-01-05 17:14:57 -0800

    integrated stripe checkout npm

--- front-end-side/src/components/pages/SingleStoryPage/SingleStoryPage.jsx
+++ front-end-side/src/components/pages/SingleStoryPage/SingleStoryPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import StripeCheckout from 'react-stripe-checkout';
 
 import { startLoadingContent } from '../../../actions';
 import './SingleStoryPage.scss';
@@ -9,8 +10,11 @@ class SingleStoryPage extends React.Component {
   constructor(props) {
     super(props);
     this.handleInitiatingStory = this.handleInitiatingStory.bind(this);
+    this.handleToken = this.handleToken.bind(this);
     this.state = {
-      isStoryStarted: false
+      isStoryStarted: false,
+      price: 1, //USD dollar,
+      BASE_URL: 'http://127.0.0.1:5000'
     }
   }
 
@@ -21,8 +25,33 @@ class SingleStoryPage extends React.Component {
     history.push(`/story/${uid}/start`)
   }
 
+  async handleToken(token) {
+    const { authInfo: { serverResponse: { user }} } = this.props;
+    const STRIPE_ENDPOINT = `${this.state.BASE_URL}/subscriptions`;
+    let headers = new Headers();
+    const jwtToken = localStorage.getItem('access_token')
+    console.log(token, 'this is token');
+    headers.set('x-access-token', jwtToken);
+
+    const obj = {
+      user_uid: user.uid,
+      payment_token: token.id
+    }
+
+    const parameters = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(obj)
+    }
+
+    const response = await fetch(STRIPE_ENDPOINT, parameters);
+    const json = await response.json();
+    console.log(json, 'this is json response')
+
+  }
+
   render() {
-    const { name, image_url, description, difficulty, length, genre } = this.props
+    const { name, image_url, description, difficulty, length, genre, is_demo } = this.props;
     return (
       <div id="story" className="page">
         <div className="story-info-container">
@@ -41,11 +70,27 @@ class SingleStoryPage extends React.Component {
           </div>
 
           <div className="buttons-container">
-            <button onClick={this.handleInitiatingStory} className="btn btn-blue">
-              Start
-            </button>
+            {is_demo === false ? 
+              <button onClick={this.handleInitiatingStory} className="btn btn-blue">
+                Start
+              </button>
+              :
+              <StripeCheckout 
+                token={this.handleToken}
+                stripeKey="pk_test_mJHbhp4HBDDMBpIfhhzZ9THz00JcFeKvRA"
+                amount={this.state.price * 100}
+                name={name}
+                description="this is description"
+                >
+                  <button className="btn btn-blue">
+                    Buy Story
+                  </button>
+              </StripeCheckout>
+
+            }
             <Link className="cancel" to="/app">Cancel</Link>
           </div>
+
         </div>
       </div>
     )
