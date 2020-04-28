e9f40537acbb032e10b8f0629b7d85484bf503cb
Kenzo Mendoza <kenzom954@gmail.com>
2020-04-27 20:04:34 -0700

    added contents for about page

--- front-end-side/src/components/pages/AboutPage/AboutPage.jsx
+++ front-end-side/src/components/pages/AboutPage/AboutPage.jsx
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export default class AboutPage extends Component {
   render() {
     return (
-      <h1>
-        About Page
-      </h1>
+      <div className="page">
+        <h1>About Us</h1>
+        <p>Our mission is to redefine language education and become the platform people use to become conversationally fluent. We are making language immersion affordable and convenient. The founders felt all language learning apps were not effective in enabling speaking fluency.
+        <br/>
+        <br/>
+        To do this, we’ve created an interactive story based approach where beginners to advanced learners of a language can practice conversations and get the feedback required to improve. We aim to create a judgment free place where you can practice and become confident in your speaking ability.
+        </p>
+      </div>
     )
   }
 }
